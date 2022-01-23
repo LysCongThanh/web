16ca0533f78b5ccbe26c65f6cfd8b310612e2346
Tuấn Duy <96539354+LysCongThanh@users.noreply.github.com>
2022-01-23 17:56:44 +0700

    first commit

--- main.js
+++ main.js
@@ -17,3 +17,95 @@ $(document).ready(function () {
         duration:2000
     });
 });
+
+//scroll Nav-bar
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
+
+//slick
+$(document).ready(function(){
+    $('.tab-content-item').hide()
+    $('.tab-content-item:first-child').fadeIn()
+    $('.nav-list li').click(function(){
+  
+        // active nav tab
+        // $('.nav-list li').removeClass('active')
+        // $(this).addClass('active')
+  
+        // show tab-content-item
+      
+        let id_tab_content = $(this).children('.nav-link').attr('href')
+        $('.tab-content-item').hide()
+        $(id_tab_content).fadeIn()
+        return false;
+    })
+  })
+
+//expanding tab
+$(document).ready(function () {
+    $('.detail').on('click', function(){
+        $(this).toggleClass('open');
+        $('.detail-content1').toggleClass('open');
+    });
+});
+
+$(document).ready(function(){
+    $('.close1').click(function(){
+        $('.detail-content1').removeClass('open')
+    })
+})
+// detail2
+$(document).ready(function () {
+    $('.detail2').on('click', function(){
+        $(this).toggleClass('open');
+        $('.detail-content2').toggleClass('open');
+    });
+});
+
+$(document).ready(function(){
+    $('.close2').click(function(){
+        $('.detail-content2').removeClass('open')
+    })
+})
+//detail3
+$(document).ready(function () {
+    $('.detail3').on('click', function(){
+        $(this).toggleClass('open');
+        $('.detail-content3').toggleClass('open');
+    });
+});
+
+$(document).ready(function(){
+    $('.close3').click(function(){
+        $('.detail-content3').removeClass('open')
+    })
+})
+//bubbly-btn
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-btn-detail");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
+//
+
+
+//aos-data
+AOS.init({
+    easing: 'ease',
+});
